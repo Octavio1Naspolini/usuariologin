b185ab9af7e8084f330466b9185019a76bc463cf
Octavio Naspolini <octavionaspolinineto@gmail.com>
2023-12-12 13:46:11 -0300

    Update TabelaEndereco.tsx

--- src/Components/TabelaEndereco/TabelaEndereco.tsx
+++ src/Components/TabelaEndereco/TabelaEndereco.tsx
@@ -11,9 +11,7 @@ interface TabelaEnderecoProps {
 const TabelaEndereco: React.FC<TabelaEnderecoProps> = ({ dados }) => {
   return (
     <div>
-    {/* Tabela para exibir o resumo do pedido */}
     <table className="resumo-tabela">
-      {/* Cabeçalho da tabela */}
       <thead>
         <tr>
           <th>Rua</th>
@@ -23,9 +21,7 @@ const TabelaEndereco: React.FC<TabelaEnderecoProps> = ({ dados }) => {
           <th>Cidade</th>
         </tr>
       </thead>
-      {/* Corpo da tabela com os dados */}
       <tbody>
-        {/* Mapeia os dados para criar linhas da tabela usando o componente ItemTabelaResumo */}
         {dados.map((item, index) => (
           <ItemTabelaEndereco key={index} item={item} />
         ))}
